6b5ca3ab1725422cfed1c2233f8ede09aba93125
Mateusz A. Kucharski <48994587+MAKucharski@users.noreply.github.com>
2020-11-26 10:29:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import numpy as np
 #import matplotlib.pyplot as plt
 import os
 import random
+import xlrd
 from numpy import unique
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 '''
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
@@ -35,18 +38,23 @@ def create_training_data():
     for category in categories:
         path = os.path.join(datadir, category)
         class_num = categories.index(category)
-        for txt in os.listdir(path):
-            txt_array = pd.read_csv(os.path.join(path, txt), sep="\t")
+        for xlsx in os.listdir(path):
+            book = xlrd.open_workbook(os.path.join(path, xlsx))
+            sheet = book.sheet_by_name('Arkusz1')
+            txt_array = [[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+            txt_array = np.array(txt_array)
+            print(type(txt_array))
+            #txt_array.to_numpy()
+            print(txt_array)
+            #txt_array.select_dtypes(include=float).to_numpy()
             training_data.append([txt_array, class_num])
-            # print(type(txt_array))
+            #print(type(training_data))
+            #print(training_data)
 
 
 create_training_data()
-# (len(training_data))
-# print(training_data)
 
 random.shuffle(training_data)
-
 # for sample in training_data:
     # print(sample[1])
 X = []  # data
@@ -56,59 +64,53 @@ for features, label in training_data:
     X.append(features)
     Y.append(label)
 
-print(type(X))
-
-X = pd.DataFrame(X).to_numpy()
 # X = X.float() ponoć ma być float potem?
+#print(X.shape) # dlaczego features jest 1? powinno być 4
+#X = X.reshape(X.shape[0], X.shape[1], 1)
+Y = np.array(Y)
+print(type(Y))
+print(Y)
 print(type(X))
-print(X.shape) # dlaczego features jest 1? powinno być 4
-print(X)
-X = X.reshape(X.shape[0], X.shape[1], 1)
-print(X.shape)
 print(X)
 X = tf.keras.utils.normalize(X)
+print(X)
+print(X.shape)
+X = X.reshape([22, 4])
+print(X.shape)
+print(X)
+X = X.reshape(22, 4, 1)
+print(X.shape)
+print(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.15)
+''' #zeby dzialalo usunac X = X.reshape(22, 4, 1)
+knn = KNeighborsClassifier(n_neighbors=5)
+
+knn.fit(X_train, y_train)
+X_new = np.array([[632.610377, 1443.110758, 2376.79294, 3404.070208]])
+X_new = tf.keras.utils.normalize(X_new)
+prediction = knn.predict((X_new))
+X_new2 = np.array([[387.371016,	1423.05739,	2334.429814, 3262.155058]])
+X_new2 = tf.keras.utils.normalize(X_new2)
+prediction2 = knn.predict((X_new2))
+print(prediction, prediction2)
+print(knn.score(X_test, y_test))
+'''
+
 
 model = Sequential()
-#model.add(Conv1D(64, (3,1), input_shape = X.shape[1:]))
-model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(22, 4)))
-model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
+model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(4, 1)))
+model.add(Dense(16, activation='relu'))
 #model.add(Activation("relu"))
-model.add(MaxPooling1D(pool_size=(2)))
-
-#model.add(Conv1D(64, (3,1)))
-model.add(Activation("relu"))
-model.add(MaxPooling1D(pool_size=(2)))
-
+model.add(MaxPooling1D())
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(3, activation='softmax'))
+#model.add(Activation('softmax'))
 
-model.add(Dense(1))
-model.add(Activation('softmax'))
-
-model.compile(loss="binary_crossentropy", optimizer="adam",
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam",
               metrics=['accuracy'])
+model.summary()
 
-model.fit(X, Y, batch_size=32, epochs=3)
-
-#a_train = pd.read_csv("C:/Users/48784/Desktop/dataset/a.txt", sep ="\t")
-#e_train = pd.read_csv("C:/Users/48784/Desktop/dataset/e.txt", sep ="\t")
-#i_train = pd.read_csv("C:/Users/48784/Desktop/dataset/i.txt", sep ="\t")
-
-#ka_test = pd.read_csv("C:/Users/48784/Desktop/dataset/test/ka.txt", sep ="\t")
-#re_test = pd.read_csv("C:/Users/48784/Desktop/dataset/test/re.txt", sep ="\t")
-
-#a_train = tf.keras.utils.normalize(a_train, axis=1)
-#e_train = tf.keras.utils.normalize(e_train, axis=1)
-#i_train = tf.keras.utils.normalize(i_train, axis=1)
-#ka_test = tf.keras.utils.normalize(ka_test, axis=1)
-#re_test = tf.keras.utils.normalize(re_test, axis=1)
-
-#model = tf.keras.models.Sequential()
-#model.add(tf.keras.layers.Flatten())
-#model.add(tf.keras.layers.dense(4, activation=tf.nn.relu))
-#model.add(tf.keras.layers.dense(4, activation=tf.nn.relu))
-#model.add(tf.keras.layers.dense(3, activation=tf.nn.softmax))
-
-#model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-#model.fit
+model.fit(X_train, y_train, batch_size=16, epochs=3, verbose=0)
+acc = model.evaluate(X_train, y_train)
+print("Loss: ", acc[0], " Accuracy: ", acc[1])
